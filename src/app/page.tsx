daf951f34af6bd27599de242216a3a947874e78e
Guilherme Guimaraes <guiweb85@gmail.com>
2025-06-23 20:24:51 -0300

    31. Criando o template das páginas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,7 @@
-import { PrimeiroComponent, ArrowFunction } from '../components/PrimeiroComponent'
 export default function Home() {
   return (
     <>
-      <PrimeiroComponent 
-          mensagem='mensagem component' 
-          mensagemDoBotao='clicou aqui ' />
 
-      <PrimeiroComponent 
-          mensagem='novo component' 
-          mensagemDoBotao='clicou aqui tambem' />
-      <ArrowFunction />
     </>
   )
 }
