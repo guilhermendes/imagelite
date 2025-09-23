69c0f1410016cf4c2d116aa485b93b7e7e3ee5d2
Guilherme Guimaraes <guiweb85@gmail.com>
2025-09-23 19:18:00 -0300

    39. Criando o formulário para pesquisa de imagens

--- src/app/galeria/page.tsx
+++ src/app/galeria/page.tsx
@@ -4,6 +4,7 @@
 import { Template, ImageCard } from "@/components"
 import { Image } from "@/resources/image/image.resource";
 import { useImageService } from '@/resources/image/image.service'
+import { Session } from "inspector/promises";
 import { useState } from "react"
 
 export default function GaleriaPage(){
@@ -33,7 +34,17 @@ export default function GaleriaPage(){
 
     return (
         <Template>
-            <button className="bg-gray-500" onClick={searchImage}>clique para mudar</button>
+            <section className="flex flex-col items-center justify-center my-5">
+                <div className="flex space-x-4">
+                    <input type="text" className="border px-3 py-2 rounded-lg text-gray-900"/>
+                    <select className="border px-4 py-2 rounded-lg text-gray-900" >
+                        <option>All formats</option>
+                    </select>
+                    <button className="bg-blue-500 text-white px-4 py-2 rounded-lg" onClick={searchImage}>Search</button>
+                    <button className="bg-yellow-500 text-white px-4 py-2 rounded-lg" >Add New</button>
+                </div>
+            </section>
+
             <section className="grid grid-cols-3 gap-8">
                 {
                     renderImageCards()
